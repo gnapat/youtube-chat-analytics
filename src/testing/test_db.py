de2930bb24c30227a60b3916fdbc50ffac77cdf2
gz <gnapat@gmail.com>
2022-11-09 12:54:12 +0700

    Add class

--- src/testing/test_db.py
+++ src/testing/test_db.py
@@ -1,3 +1,25 @@
 
 import sys
+#sys.path.insert(0, '/home/gz/project/nida/ychat/youtube-chat-analytics/src/include/')
+sys.path.insert(0, 'src/include/')
+
+from ychat_db  import ychatdb
+
+la = {"host":"mongodb+srv://appadm:e7Eddf7fe7Eddf7f@cluster0.g7t88xt.mongodb.net/?retryWrites=true&w=majority",
+        "database":"YouTubeChat"}
+client = ychatdb(la)
+#client.getCollection("YouTubeChat")
+
+ret = client.getDocument("A00011")
+cc = 0
+for i in ret:
+    if (cc > 9):
+        break
+
+    print(i)
+
+    cc+=1
+
+
+
 
