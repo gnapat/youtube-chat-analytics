5a9d7a6f3a9c24a411c0f102dcfaa251d5424d05
gnapat <gnapat@gmail.com>
2022-11-29 23:49:12 +0700

    ss

--- src/include/ychat_db.py
+++ src/include/ychat_db.py
@@ -1,24 +1,102 @@
 
 import sys
 from pymongo import MongoClient
+import pymongo
 
 class ychatdb:
     def __init__(self,param) -> None:
         self.host= param['host']
         self.db_name = param['database']
-        #print(self.host)
-        #client = MongoClient("mongodb+srv://appadm:e7Eddf7fe7Eddf7f@cluster0.g7t88xt.mongodb.net/?retryWrites=true&w=majority" , 
-        #                    tls=True,
-        #                    tlsAllowInvalidCertificates=True)
 
-        client = MongoClient( self.host, 
+        try:
+            client = MongoClient( self.host, 
                             tls=True,
                             tlsAllowInvalidCertificates=True)
+        except pymongo.errors.ConnectionFailure as e:
+            print ("Could not connect: %s" %(e))
+        except pymongo.errors.ServerSelectionTimeoutErro as e:
+             print ("Could not connect: %s" %(e))
+        except:
+            print("Error!!!!??")
+             
+
                             
         print("Test")
         self.client = client
         self.db=client[self.db_name]
         #return(client)
+    
+    def addNewChannel(self,code,name):
+        info=self.db['info']
+        #print(f"Check {code}")
+        ret = info.find({'chl_code':code})
+
+        cc=0
+        alist = []
+        for i in ret:
+            alist.append(i)
+            cc += 1
+        
+        if alist !=[]:
+            return(-1)
+        else:
+            record={'chl_code':code,'title_th':name,'title_en':''}
+            info.insert_one(record)
+            return(0)
+
+    def addNewProgram(self,code,name,program):
+        colch = self.db['cliplist']
+        record={"code":code,"vid":"9GXSL5bgPII","org_name_th":name,"org_name_en":"","title_th":program,"title_en":""}
+        colch.insert_one(record)
+
+    def putMsg(self,code,name,program,record_list):
+        col = self.db[code]
+        colch = self.db['cliplist']
+        cc = 0
+        ssize = 10
+        l = []
+        for i in record_list:
+            record={"datetime":i['datetime'],"vid":i['vid'],"aname":i['aname'],"msg":i['msg']}
+            l.append(record)
+            cc += 1
+        
+        col.insert_many(l)
+
+        data = l[0]
+        record={"code":code,"vid":data['vid'],"org_name_th":name,"org_name_en":"","title_th":program,"title_en":""}
+        colch.insert_one(record)
+
+
+
+
+
+
+
+    def removeChannel(self,code):
+        info=self.db['info']
+        print(f"Check {code}")
+        ret = info.find({'chl_code':code})
+
+        cc=0
+        alist = []
+        for i in ret:
+            alist.append(i)
+            
+            cc += 1
+        
+        if alist !=[]:
+            record={'chl_code':code}
+            info.delete_one(record)
+            return(1)
+        else:
+
+            return(0)
+        
+
+        #for i in ret:
+        #    print(i)
+        #record={'chl_code':code,'title_th':name,'title_en':''}
+        #info.insert_one(record)
 
     def getDatabases(self):
         return(self.client.list_database_names)
@@ -44,7 +122,16 @@ class ychatdb:
 
     def getChList(self):
         collec = self.db['info']
-        re = collec.find({})
+        try:
+            re = collec.find({})
+        except pymongo.errors.ExecutionTimeout as e:
+             print ("Could not find: %s" %(e))
+             re = None
+        except pymongo.errors.ServerSelectionTimeoutErro as e:
+             print ("Could not connect: %s" %(e))
+        except:
+            print("Error!!!!")
+        
         return(re)
     
     def getChCodeByTitleTh(self,name):
@@ -65,17 +152,34 @@ class ychatdb:
 
         re = colch.find({"org_name_th":chname,'title_th':ep[0]})
         r =re[0]
-        #for i in re:
-        #    print(i)
-        aa={"org_name_th":chname,'title_th':ep[0]}
+
         col = self.db[r['code']]
         chdata = col.find({"vid":r['vid']})
         return(chdata)
 
+    def getCodeByCh(self,chname):
+        collec = self.db['info']
+        #colch = self.db['cliplist']
+
+        re = collec.find({"title_th":chname})
+        r =re[0]
+        #print(r)
+        return(r['chl_code'])
+
+    def renameProgram(self,chname,curr_progm,new_progm):
+        collec = self.db['info']
+        colch = self.db['cliplist']
+
+
+        re = colch.find({"org_name_th":chname,'title_th':curr_progm})
+        ri = re[0]
+        colch.update_one({'_id':ri['_id']}, {"$set": {"title_th":new_progm }})
+
+
 
         
 
-
+    
 
     
 
--- src/viewer.py
+++ src/viewer.py
@@ -3,11 +3,12 @@ import PySimpleGUI as sg
 import sys
 
 from matplotlib.ticker import NullFormatter  # useful for `logit` scale
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt, dates
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import matplotlib
-from datetime import datetime
+from datetime import datetime, timedelta
 
 sys.path.insert(0, 'src/include/')
 
@@ -16,14 +17,15 @@ from config import ychat_config as yconf
 import pandas as pd
 
 
-
+NUMOFPLOT = 1
 
 use_custom_titlebar = False
 
 class AppSystem:
 
     def __init__(self) -> None:
-        self.yc = yconf("C:\\Users\\LEGION\\work\\project\\nida\\youtube-chat-analytics\\src\\testing\\config.conf")
+        #self.yc = yconf("C:\\Users\\LEGION\\work\\project\\nida\\youtube-chat-analytics\\src\\testing\\config.conf")
+        self.yc = yconf("src\\config.conf")
         self.dblist = self.yc.getconfig()
 
 
@@ -39,7 +41,7 @@ def draw_figure(canvas, figure):
     return figure_canvas_agg
 
 def draw_clear(window):
-    fig, axs = plt.subplots(2, 1, figsize=(6, 6), constrained_layout=True)
+    fig, axs = plt.subplots(NUMOFPLOT, 1, figsize=(6, 6), constrained_layout=True)
 
     axs[0].set_title("NONE")
 
@@ -55,9 +57,19 @@ def delete_figure_agg(figure_agg):
         print(f'Error removing {figure_agg} from list', e)
     plt.close('all')
 
-def MessageBox(topic,message):
+def MessageBoxError(topic,message):
     #sg.popup('Go button clicked', 'Input value:', self.values['-IN-'])
-    sg.popup(topic, message)
+    #sg.popup(topic, message)
+    sg.popup_error(topic,message,keep_on_top=True)
+
+def MessageBoxInfo(topic,message):
+    #sg.popup('Go button clicked', 'Input value:', self.values['-IN-'])
+    #sg.popup(topic, message)
+    sg.popup_ok(topic,message,keep_on_top=True)
+def MessageBoxWarn(topic,message):
+    #sg.popup('Go button clicked', 'Input value:', self.values['-IN-'])
+    #sg.popup(topic, message)
+    sg.popup_notify(topic,message,keep_on_top=True)
 
 
 def make_window(theme=None):
@@ -84,25 +96,105 @@ def make_window(theme=None):
         return sg.Text(name, size=(len(name)+1,1), justification='l',pad=(0,0), font='Loma 10')
 
     font = ("Norasi", 11)
-    layout_l = [[name('Channel'), sg.Combo(chlist, default_value=[], s=(15,40), enable_events=True, readonly=True,font=font,k='-CHCOMBO-')],
-                [sg.Listbox([], no_scrollbar=True,  s=(40,30),k='-EP_LIST-')]]
 
-    layout_c=[sg.Button('Apply'), 
-                sg.Button('Clean')],
-    layout_r = [[name('Channel')] ,[sg.Canvas(key='-PLOT01-')]]
 
-    layout_main=[[sg.T('YouTube Chat Simple Analytics', font='Norasi 18', justification='c', expand_x=True)],
-                [[name('Database:'), sg.Combo(dblist, default_value=dblist[0], s=(15,30), enable_events=False, readonly=True, k='-DBCOMBO-')]],
+    layout_l = [[sg.T('YouTube Live Chat Viewer', font='Norasi 18', justification='c', expand_x=True)],
+                [name('Database:'), sg.Combo(dblist, default_value=dblist[0], s=(25,30), enable_events=False, readonly=True, k='-DBCOMBO-')],
                 [sg.Button('Connect'), sg.Button('Disconnect')],
-                [sg.Col(layout_l),sg.Col(layout_c), sg.Col(layout_r)]]
+                [name('Channel'), sg.Combo(chlist, default_value=[], s=(15,40), enable_events=True, readonly=True,font=font,k='-CHCOMBO-'),sg.Text(size=20,k='-CHCODE-')],
+                [sg.T('Program', font='Norasi 12', justification='l', expand_x=True)],
+                [sg.Listbox([], no_scrollbar=True,  s=(50,15),k='-EP_LIST-')],
+                [sg.T('Program Manager', font='Norasi 12', justification='l', expand_x=True)],
+                [sg.Button('Get Program'),sg.Button('Rename')],
+                [sg.Input(k='-Program-',s=50)], 
+                [sg.T('Channel Manager', font='Norasi 12', justification='l', expand_x=True)],
+                [sg.Button('Add New Channel'),sg.Button('Remove Channel')],
+                [name('Channel Code'),sg.Input(k='-AddChCode-',s=20)],
+                [name('Channel Name'),sg.Input(k='-AddChName-',s=35)],
+                [sg.T('Clone Program', font='Norasi 12', justification='l', expand_x=True)],
+                [name('Clone to:'),sg.Combo(dblist, default_value=dblist[0], s=(25,30), enable_events=False, readonly=True, k='-CLONEDBCOMBO-'),sg.Button('Submit Clone')] ]
+    layout_c=[[sg.Button('Apply')], [sg.Button('Clean')]]
 
 
-    window = sg.Window('YouTube Chat Simple Analytics', layout_main, finalize=True, \
+    layout_r = [[sg.Canvas(key='-PLOT01-')]]
+    
+    layout_main=[[sg.Col(layout_l),sg.Col(layout_c), sg.Col(layout_r)]]
+    
+    window = sg.Window('YouTube Live Chat Viewer', layout_main, finalize=True, \
         right_click_menu=sg.MENU_RIGHT_CLICK_EDITME_VER_EXIT, keep_on_top=True, use_custom_titlebar=use_custom_titlebar)
     
 
 
     return window
+
+def ToVirtualize(client,ch,ep):
+    print(f"{ch}  {ep}")
+    re = client.getChByTitleTh(ch,ep)
+    a=[]; b=[]
+    hh=0; mm=0
+    cc = 0
+    for i in re:
+        t=i['datetime']
+
+        if cc ==0:
+            hh=int(t[11:13]); mm=int(t[14:16])
+
+        a.append([datetime.strptime(t,'%Y-%m-%d %H:%M:%S'),1])
+        b.append([datetime.strptime(t,'%Y-%m-%d %H:%M:%S'),i['aname']])
+
+        cc += 1
+            
+    df_user_count = pd.DataFrame({'timeplay': np.array([i - timedelta(hours=hh,minutes=mm)  for i,j in a]),
+                                'user': np.array([j for i,j in a])})
+    plt.rcParams['font.family'] = 'Tahoma'
+    try:
+        group_user_count = df_user_count.groupby(pd.Grouper(key='timeplay', axis=0, freq='1min')).sum()
+        mean_per_min = group_user_count['user'].mean()
+        sum = group_user_count['user'].sum()
+
+    except Exception as e:
+        print('Error Group By Dataframe', e)
+                
+        fig, axs = plt.subplots(NUMOFPLOT, 1, figsize=(8, 6), constrained_layout=True)
+        axs.set_title("")
+        axs.grid()
+        #figure_agg = draw_figure(window['-PLOT01-'].TKCanvas, fig)
+        MessageBoxError("Data Error","Sorry, Program found some data error!!")
+        return(fig)
+    else:
+        fig, axs = plt.subplots(NUMOFPLOT, 1, figsize=(8, 6), constrained_layout=True)
+        axs.plot(group_user_count)
+        axs.xaxis.set_major_formatter(dates.DateFormatter('%H:%M'))
+        detail="avg = %0.2f message/minute, total = %d" %(mean_per_min,sum)
+        axs.legend([detail], loc='upper center')
+        axs.set_xlabel('time (Hour)')
+        axs.set_ylabel('messages per minute')
+        axs.xaxis.set_tick_params(rotation=10, labelsize=10)
+        axs.set_title(ep)
+        axs.grid()
+
+        return(fig)
+    #figure_agg =draw_figure(window['-PLOT01-'].TKCanvas, fig)
+
+def UpdateValueOnDisplay(client,window):
+
+    al=[]
+    at = client.getChList()
+    for  i in at:
+        al.append(i['title_th'])
+
+        window['-CHCOMBO-'].update(values=al, value='',set_to_index=0)
+
+        re = client.getChCodeByTitleTh(al[0])
+        eplist=[]
+        for j in re:
+            eplist.append(j['title_th'])
+        
+        #if eplist != []:
+        window['-EP_LIST-'].update(values=eplist,set_to_index=0)
+        window['-Program-'].update(value="")
+
+
 
 
 def main():
@@ -113,7 +205,7 @@ def main():
     App = AppSystem()
     window = make_window()
 
-    fig, axs = plt.subplots(1, 1, figsize=(8, 6), constrained_layout=True)
+    fig, axs = plt.subplots(NUMOFPLOT, 1, figsize=(8, 6), constrained_layout=True)
     axs.set_title("")
     figure_agg = draw_figure(window['-PLOT01-'].TKCanvas, fig)
 
@@ -134,70 +226,29 @@ def main():
                 dbconf={"host":dbc['host'],"database":dbc['database']}
 
             except Exception as e:
-                MessageBox("Database fail,", "Please try again!!")
+                MessageBoxError("Database fail,", "Please try again!!")
                 continue
             else:
                 pass
 
             print("Connect %s %s" %(values['-DBCOMBO-'], dbc['host']))
             client = ychatdb(dbconf)
-            al=[]
-            at = client.getChList()
-            for  i in at:
-                al.append(i['title_th'])
 
-            window['-CHCOMBO-'].update(values=al, value='')
+            UpdateValueOnDisplay(client,window)
 
         elif event == "Disconnect":
             print("Disconnect")
+
+
         elif event == "Apply":
             ch = values['-CHCOMBO-']
             ep = values['-EP_LIST-']
             
-            #client.getChCodeByTitleTh(ep)
-            re = client.getChByTitleTh(ch,ep)
-            #print(f"Apply  {re['vid']}")
-            a=[]
-            b=[]
-            for i in re:
-                t=i['datetime']
-                a.append([datetime.strptime(t,'%Y-%m-%d %H:%M:%S'),1])
-                b.append([datetime.strptime(t,'%Y-%m-%d %H:%M:%S'),i['aname']])
-            
-            df_user_count = pd.DataFrame({'timeplay': np.array([i for i,j in a]),
-                          'user': np.array([j for i,j in a])})
-
-            #df_user = pd.DataFrame({'timeplay': np.array([i for i,j in b]),
-            #              'user': np.array([j for i,j in b])})
-                          
-            #df_user
-            #df_sort_user = df_user.groupby('user',sort=False)['user'].count().sort_values(ascending=False).iloc[0:10] 
-
-            #group_count = np.array([i for i in df_sort_user])
-            #group_name = np.array([i for i in df_sort_user.keys()])
-
             delete_figure_agg(figure_agg)
-            plt.rcParams['font.family'] = 'Tahoma'
+            fig = ToVirtualize(client,ch,ep)
 
-            
-            try:
-                group_user_count = df_user_count.groupby(pd.Grouper(key='timeplay', axis=0, freq='1min')).sum()
+            figure_agg = draw_figure(window['-PLOT01-'].TKCanvas, fig)
 
-            except Exception as e:
-                print('Error Group By Dataframe', e)
-                
-                fig, axs = plt.subplots(1, 1, figsize=(8, 6), constrained_layout=True)
-                axs.set_title("")
-                figure_agg = draw_figure(window['-PLOT01-'].TKCanvas, fig)
-                MessageBox("Data Error","Sorry, Program found some data error!!")
-            else:
-                fig, axs = plt.subplots(1, 1, figsize=(8, 6), constrained_layout=True)
-                axs.plot(group_user_count)
-                axs.set_xlabel('time (minute)')
-                axs.set_ylabel('frequency (minute)')
-                axs.set_title(ep)
-                figure_agg =draw_figure(window['-PLOT01-'].TKCanvas, fig)
-    
 
         elif event == "Clean":
             print("Clean Board")
@@ -211,11 +262,100 @@ def main():
             for i in re:
                 eplist.append(i['title_th'])
 
-            window['-EP_LIST-'].update(values=eplist)
+            window['-EP_LIST-'].update(values=eplist,set_to_index=0)
+
+        elif event == "-EP_LIST-":
+            print("-------------")
+
+        elif event == "Rename":
+            new_progm = values['-Program-']
+            curr_progm = values['-EP_LIST-'][0]
+            ch = values['-CHCOMBO-']
+
+            if (len(new_progm)>0):
+                client.renameProgram(ch,curr_progm,new_progm)
+                UpdateValueOnDisplay(client,window)
+            else:
+                MessageBoxError("Rename","Please enter new program name.")
+            
+            #print(f"{values['-Program-']}")
+
+        elif event == "Get Program":
+            window['-Program-'].update(value=values['-EP_LIST-'][0])
+
+        elif event == "Add New Channel":
+
+            chcode = values['-AddChCode-']
+            chname = values['-AddChName-']
+
+
+            if (len(chcode)>0) and (len(chname)):
+                
+                ret = client.addNewChannel(chcode,chname)
+                if ret <0:
+                    MessageBoxError("New Channel","This code is exist!!")
+                else:
+
+                    MessageBoxInfo("New Channel","Add new channel Success.")
+                    UpdateValueOnDisplay(client,window)
+
+
+            else:
+                MessageBoxError("New Channel","Please enter complete information.")
+        
+        elif event == "Remove Channel":
+
+            chcode = values['-AddChCode-']
+
+
+            if (len(chcode)>0):
+                ret = client.removeChannel(chcode)
+                if ret >0:
+                    MessageBoxInfo("New Channel","Remove Success")
+                    UpdateValueOnDisplay(client,window)
+                else:
+
+                    MessageBoxError("New Channel","Not found channel")
+
+
+            else:
+                MessageBoxError("New Channel","Please enter complete information.")
+
+        elif event == "Submit Clone":
+            ch = values['-CHCOMBO-']
+            progm = values['-EP_LIST-']
+            db_des = values['-CLONEDBCOMBO-']
+            db_cur = values['-DBCOMBO-']
+            
+            if db_des == db_cur:
+                MessageBoxInfo("Submit Clone","Can't clone to same database.")
+            else:
+                dbl = App.getDblist()
+                dbc = dbl[values['-CLONEDBCOMBO-']]
+                dbc_dest={"host":dbc['host'],"database":dbc['database']}
+           
+                client_dest = ychatdb(dbc_dest)
+                retAllMsg = client.getChByTitleTh(ch,progm)
+                
+                
+                chcode_curr = client.getCodeByCh(ch)
+                client_dest.addNewChannel(chcode_curr,ch)
+                client_dest.putMsg(chcode_curr,ch,progm,retAllMsg)
+
+                MessageBoxInfo("Submit Clone","Success")
+
+                
+                
+                
+
+
+
+            
+
 
 
         
-    print("Hello World!")
+    print("Bye..")
 
 if __name__ == "__main__":
     main()
